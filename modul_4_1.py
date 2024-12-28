a18542e94e28101483ec57f3ae1d541414faf65a
Andrey Podianov <podyanov82@gmail.com>
2024-12-28 23:37:25 +0500

    Задача "А как делить?"

--- modul_4_1.py
+++ modul_4_1.py
@@ -3,12 +3,10 @@
 from true_math import divide as divt
 from fake_math import divide as divf
 
-
 result1 = divf(69, 3)
 result2 = divf(3, 0)
 result3 = divt(49, 7)
 result4 = divt(15, 0)
-
 
 print(result1)
 print(result2)
